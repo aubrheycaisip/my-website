d3a7b7dc8b37504d31ac22f05858cee883b252ab
Aubrhey <aubrhey.caisip@ph.ncs-i.com>
2022-05-04 17:21:00 +0800

    create project item card

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -3,6 +3,7 @@ import { projects } from "../../constant/Projects.js";
 import "./Project.css";
 import ProgressBar from "./ProgressBar.js";
 import ProjectItem from "./ProjectItem.js";
+import ProjectCard from "./ProjectCard.js";
 
 class Project extends React.Component {
     render() {
@@ -16,62 +17,78 @@ class Project extends React.Component {
         ];
         return (
             <div className='project-div'>
-                {projects.map((project, index) => (
-                    <div className='project-list-div' key={index}>
-                        <div className='project-bar-bg'></div>
-                        <div
-                            style={{
-                                backgroundImage: `url(${project.backgroundPic})`,
-                                borderRadius: "25px",
-                                padding: "50px 50px",
-                                backgroundRepeat: "no-repeat",
-                                backgroundSize: "cover",
-                                backgroundPosition: "center",
-                                margin: "-510px 25px 10px",
-                                boxShadow: "1px 2px 1px #E6ECEA",
-                                maxWidth: "1224px",
-                            }}
-                        >
-                            <div className='project-inner-container'>
-                                <img
-                                    src={project.logo}
-                                    alt={project.projectName}
-                                />
-                                <div className='project-information-div'>
-                                    <h1>{project.projectName}</h1>
-                                    <p>{project.projectDescription}</p>
-                                    <p>
-                                        <strong>Assignment:</strong>{" "}
-                                        {project.assignment}
-                                    </p>
-                                    <p>
-                                        <strong>Tools:</strong> {project.tools}
-                                    </p>
-                                    <div
-                                        style={{
-                                            display: "flex",
-                                        }}
-                                    >
-                                        <p
+                <div style={{ display: "none" }}>
+                    {projects.map((project, index) => (
+                        <div className='project-list-div' key={index}>
+                            <div className='project-bar-bg'></div>
+                            <div
+                                style={{
+                                    backgroundImage: `url(${project.backgroundPic})`,
+                                    borderRadius: "25px",
+                                    padding: "50px 50px",
+                                    backgroundRepeat: "no-repeat",
+                                    backgroundSize: "cover",
+                                    backgroundPosition: "center",
+                                    margin: "-510px 25px 10px",
+                                    boxShadow: "1px 2px 1px #E6ECEA",
+                                    maxWidth: "1224px",
+                                }}
+                            >
+                                <div className='project-inner-container'>
+                                    <img
+                                        src={project.logo}
+                                        alt={project.projectName}
+                                    />
+                                    <div className='project-information-div'>
+                                        <h1>{project.projectName}</h1>
+                                        <p>{project.projectDescription}</p>
+                                        <p>
+                                            <strong>Assignment:</strong>{" "}
+                                            {project.assignment}
+                                        </p>
+                                        <p>
+                                            <strong>Tools:</strong>{" "}
+                                            {project.tools}
+                                        </p>
+                                        <div
                                             style={{
-                                                margin: "0 15px 0 0",
+                                                display: "flex",
                                             }}
                                         >
-                                            <strong>Work:</strong>{" "}
-                                        </p>
-                                        <ProgressBar
-                                            percentage={project.work}
-                                            label={`${project.work}%`}
-                                        />
+                                            <p
+                                                style={{
+                                                    margin: "0 15px 0 0",
+                                                }}
+                                            >
+                                                <strong>Work:</strong>{" "}
+                                            </p>
+                                            <ProgressBar
+                                                percentage={project.work}
+                                                label={`${project.work}%`}
+                                            />
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                    ))}
+                    <div style={{ display: "none" }}>
+                        {listItem.map((item, key) => (
+                            <ProjectItem project={item} key={key} />
+                        ))}
                     </div>
-                ))}
-                <div style={{ display: "none" }}>
-                    {listItem.map((item, key) => (
-                        <ProjectItem project={item} key={key} />
+                </div>
+
+                <div
+                    style={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        padding: "5px 10px",
+                        justifyContent: "center",
+                    }}
+                >
+                    {projects.map((project, key) => (
+                        <ProjectCard project={project} key={key} />
                     ))}
                 </div>
             </div>
---
+++ src/components/Project/ProjectCard.js
@@ -0,0 +1,35 @@
+import "./ProjectCard.css";
+
+const ProjectCard = (props) => {
+    return (
+        <div className='container'>
+            <div className='card'>
+                <div className='imgBx'>
+                    <img
+                        src={props.project.logo}
+                        alt={props.project.projectName}
+                    />
+                </div>
+                <div className='contentBx'>
+                    <h2>Nike Shoes</h2>
+                    <div className='size'>
+                        <h3>Size :</h3>
+                        <span>7</span>
+                        <span>8</span>
+                        <span>9</span>
+                        <span>10</span>
+                    </div>
+                    <div className='color'>
+                        <h3>Color :</h3>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    {/* <a href='#'>Buy Now</a> */}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProjectCard;
